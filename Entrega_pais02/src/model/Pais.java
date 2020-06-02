55cad0284a6d471eee8f1bbc172e7565c9581c39
cesaromera <61953097+cesaromera@users.noreply.github.com>
2020-06-02 13:22:42 -0300

    Entrega 02
    Projeto País - versão mais atualizada 02.06.2020

---
+++ Entrega_pais02/src/model/Pais.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.io.Serializable;
+
+public class Pais implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String nome;
+	private long populacao;
+	private double area;
+
+	public Pais(int id, String nome, long populacao, double area) {
+		this.id = id;
+		this.nome = nome;
+		this.populacao = populacao;
+		this.area = area;
+	}
+
+	public Pais() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+
+	public long getPopulacao() {
+		return populacao;
+	}
+
+	public void setPopulacao(long populacao) {
+		this.populacao = populacao;
+	}
+
+	@Override
+	public String toString() {
+		return "Paises [id= " + id + ", nome= " + nome + ", populacao= " + populacao + ", area= " + area + "]";
+	}
+
+}
